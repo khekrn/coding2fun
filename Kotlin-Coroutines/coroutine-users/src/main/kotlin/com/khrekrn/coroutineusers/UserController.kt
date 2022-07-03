ac1b0837ca59b8f660a33f6e9638f8ba70156fda
khekrn <kishorenayark@gmail.com>
2022-07-03 22:19:09 +0530

    Initial Commit Coroutines

--- Kotlin-Coroutines/coroutine-users/src/main/kotlin/com/khrekrn/coroutineusers/UserController.kt
+++ Kotlin-Coroutines/coroutine-users/src/main/kotlin/com/khrekrn/coroutineusers/UserController.kt
@@ -2,7 +2,10 @@ package com.khrekrn.coroutineusers
 
 import com.khrekrn.coroutineusers.storage.User
 import com.khrekrn.coroutineusers.storage.UserRepo
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.withContext
 import org.springframework.web.bind.annotation.*
 
 data class UserModel(val name: String, val emailId: String)
@@ -14,9 +17,11 @@ class UserController(private val userRepo: UserRepo) {
     @PostMapping("/reactor/user")
     @ResponseBody
     suspend fun storeUser(@RequestBody model: UserModel, @RequestParam(required = true) wait: Long): User {
-        delay(wait)
-        val userEntity = User(name = model.name, emailId = model.emailId)
-        return userRepo.save(userEntity)
+        return withContext(Dispatchers.IO + SupervisorJob()){
+            delay(wait)
+            val userEntity = User(name = model.name, emailId = model.emailId)
+            userRepo.save(userEntity)
+        }
     }
 }
 
